d3857c6878218f81eb3b4ebc2c16d33238f25d0a
AnkitaKumari28 <110804221+AnkitaKumari28@users.noreply.github.com>
2023-01-16 18:32:26 +0530

    Add files via upload
    Assessment2

---
+++ ass/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PersonComponent } from './person/person.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+const routes: Routes = [
+  {path:'person', component:PersonComponent},
+  {path:'student', component:StudentListComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
