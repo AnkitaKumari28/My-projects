adacac23f7c4cb5dbbbb3df8c76164d666348046
AnkitaKumari28 <110804221+AnkitaKumari28@users.noreply.github.com>
2023-01-16 18:34:06 +0530

    Add files via upload

---
+++ Assessment3/ass/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { EmpDataService } from './Services/Employee/emp-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    EmployeeListComponent,
+    ViewComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(EmpDataService),
+    MatTableModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
